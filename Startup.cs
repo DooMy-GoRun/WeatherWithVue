c6aa0f4d0315e309b0251508b1f38dc8128c2388
DooMy <gorun_a@hotmail.com>
2021-06-14 12:21:53 +0300

    Добавил клиента с Vue.js Установил поверх плагин vuetify. Через Nuget установил VueMiddleWare. Изменил код в Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VueCliMiddleware;
 
 namespace WeatherWithVue
 {
@@ -24,8 +25,11 @@ namespace WeatherWithVue
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,12 +41,27 @@ namespace WeatherWithVue
             }
 
             app.UseRouting();
+            app.UseSpaStaticFiles();
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSpa(spa =>
+            {
+                if (env.IsDevelopment())
+                    spa.Options.SourcePath = "ClientApp/";
+                else
+                    spa.Options.SourcePath = "dist";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseVueCli(npmScript: "serve");
+                }
+
             });
         }
     }
