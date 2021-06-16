1546e810a9469eaaf14db753ce79420176afa1cf
DooMy <gorun_a@hotmail.com>
2021-06-16 16:39:47 +0300

    Реализовал вывод данных с сервера через потоки.

--- clientapp/src/App.vue
+++ clientapp/src/App.vue
@@ -1,33 +1,33 @@
 <template>
-    <div id="app">
-        <v-app>
+    <v-app>
+        <div id="app">
+
             <v-toolbar dark
                        prominent
                        src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
-                <v-toolbar-title>Погода в городе N на ближайшие 10 дней:</v-toolbar-title>
+                <v-toolbar-title id="tableLabel">Погода в городе N на ближайшие 10 дней:</v-toolbar-title>
                 <v-spacer></v-spacer>
-                <v-btn @click="refreshinfo">Обновить данные</v-btn>
+                <v-btn @click="refreshInfo">Обновить данные</v-btn>
             </v-toolbar>
             <p v-if="!forecasts"><em>Loading...</em></p>
-            <v-data-table :headers="headers"
-                          :items="forecasts"
-                          :items-per-page="10"
-                          hide-default-footer>
-               <!--<template slot="headers">
-                   <tr>
-                       <th>Date</th>
-                       <th>Temp.Min (C)</th>
-                       <th>Temp.Max (C)</th>
-                       <th>Pressure (mm Hg)</th>
-                       <th>Humidity (%)</th>
-                       <th>Wind speed (m/sec)</th>
-                       <th>Wind Direction</th>
-                       <th>Summary</th>
-                   </tr>
-                 </template>-->
-
-
-               <!--<template slot="items">
+            <!--<v-data-table :headers="headers"
+                  :items="forecasts"
+                  :items-per-page="10"
+                  hide-default-footer>-->
+            <table class='table table-striped' aria-labelledby="tableLabel" v-if="forecasts">
+                <thead>
+                    <tr>
+                        <th>Дата</th>
+                        <th>Температура (C)</th>
+                        <th>-(C) Температура</th>
+                        <th>Давление (мм.рт.ст).</th>
+                        <th>Влажность (%)</th>
+                        <th>Сила ветра (м/с)</th>
+                        <th>Направление ветра</th>
+                        <th>Осадки</th>
+                    </tr>
+                </thead>
+                <tbody>
                     <tr v-for="forecast of forecasts" v-bind:key="forecast">
                         <td>{{ forecast.date }}</td>
                         <td>{{ forecast.temperatureMin }}</td>
@@ -38,15 +38,25 @@
                         <td>{{ forecast.windDirection }}</td>
                         <td>{{ forecast.summary }}</td>
                     </tr>
-                </template>-->
+                </tbody>
+            </table>
 
 
-            </v-data-table>
-        </v-app>
-    </div>
+            <!-- </v-data-table>-->
+
+        </div>
+    </v-app>
 </template>
 
-
+<style>
+    #app {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
 
 <script>
     import axios from 'axios'
@@ -54,7 +64,7 @@
         name: "App",
         data() {
             return {
-                headers: [
+               /* headers: [
                     {
                         text: 'Дата',
                         align: 'start',
@@ -68,7 +78,7 @@
                     { text: 'Сила ветра (м/с)', value: 'WindSpeed' },
                     { text: 'Направление ветра', value: 'WindDirection' },
                     { text: 'Осадки', value: 'Summary' },
-                ], 
+                ], */
                 forecasts: []
             }
         },
@@ -86,7 +96,7 @@
         mounted() {
             this.getWeatherForecasts();
         },
-        refreshinfo() {
+        refreshInfo() {
             this.getWeatherForecasts();
         }
     }
